5a80fd572921863ce38b8bf50db8d12364a0e8a9
Anthony G <egiulia0@email.cpcc.edu>
2023-02-26 16:31:33 -0500

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -22,14 +22,14 @@ public class Policy
     }
 
     /** Argument accepting constructor 
-     * @param policyNum User's insurance policy number
-     * @param providerName User's insurance provider
-     * @param holderFirstName Policyholder's first name
-     * @param holderLastName Policyholder's last name
-     * @param holderAge Policyholder's age
-     * @param holderSmoker Policyholder's smoking status (smoker or non-smoker)
-     * @param holderHeight Policyholder's height
-     * @param holderWeight Policyholder's weight
+     * @param pNum User's insurance policy number
+     * @param provName User's insurance provider
+     * @param first Policyholder's first name
+     * @param last Policyholder's last name
+     * @param age Policyholder's age
+     * @param smoker Policyholder's smoking status (smoker or non-smoker)
+     * @param height Policyholder's height
+     * @param weight Policyholder's weight
     */
     public Policy(int pNum, String provName, String first, String last, int age, boolean smoker, double height, double weight)
     {
@@ -43,81 +43,129 @@ public class Policy
         holderWeight = weight;
     }
 
-    /** Methods to set & get policy number */
+    /** Method to set policy number
+     * @param p User's policy number 
+     */
     public void setPolicyNumber(int p)
     {
         policyNum = p;
     }
+
+    /** Method to get policy number
+     * @return Returns the user's policy number
+     */
     public int getPolicyNumber()
     {
         return policyNum;
     }
 
-    /** Methods to set & get name of insurance provider  */
+    /** Method to set name of insurance provider
+     * @param pName Insurance provider's name
+      */
     public void setProviderName(String pName)
     {
         providerName = pName;
     }
+
+    /** Method to get insurance provider's name
+     * @return Returns user's insurance provider name
+     */
     public String getProviderName()
     {
         return providerName;
     }
 
-    /** Set & get policyholder's first name */
+    /** Sets policyholder's first name
+     * @param hFirst User's first name
+     */
     public void setFirstName(String hFirst)
     {
         holderFirstName = hFirst;
     }
+
+    /** Method to get user's first name
+     * @return Returns user's first name
+     */
     public String getFirstName()
     {
         return holderFirstName;
     }
 
-    /** Set & get policyholder's last name */
+    /** Set policyholder's last name
+     * @param hLast Policy holder's last name
+     */
     public void setLastName(String hLast)
     {
         holderLastName = hLast;
     }
+
+    /** Get user's last name
+     * @return Returns policyholder's last name
+     */
     public String getLastName()
     {
         return holderLastName;
     }
 
-    /** Set & get holder's age */
+    /** Set holder's age
+     * @param a Policyholder's age
+     */
     public void setAge(int a)
     {
         holderAge = a;
     }
+
+    /** Get user's age
+     * @return Returns policyholder's age
+     */
     public int getAge()
     {
         return holderAge;
     }
 
-    /** Set & get smoking status */
+    /** Set smoking status
+     * @param s Policyholder's smoking status
+     */
     public void setSmoker(boolean s)
     {
         holderSmoker = s;
     }
+
+    /** Get smoking status
+     * @return Return's policyholder's smoking status
+     */
     public boolean getSmoker()
     {
         return holderSmoker;
     }
 
-    /** Set & get height */
+    /** Set height
+     * @param h Policyholder's height
+     */
     public void setHeight(double h)
     {
         holderHeight = h;
     }
+
+    /** Get height
+     * @return Policyholder's height
+     */
     public double getHeight()
     {
         return holderHeight;
     }
 
-    /** Set & get weight */
+    /** Set weight
+     * @param w Policyholder's weight
+     */
     public void setWeight(double w)
     {
         holderWeight = w;
     }
+
+    /** Get weight
+     * @return Returns policyholder's weight
+     */
     public double getWeight()
     {
         return holderWeight;
@@ -132,7 +180,9 @@ public class Policy
     }
 
     /** Calculates and returns insurance policy price
-     * based on variables determined by user input */
+     * based on variables determined by user input
+     * @param b Policyholder's BMI
+     * @return Returns Policyholder's calculated price for policy */
     public double policyCost(double b)
     {
         double bmi = b;
