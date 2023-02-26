8fbf3ea1b6b25b50b53835c3c872bb6700d32c53
Anthony G <egiulia0@email.cpcc.edu>
2023-02-26 17:25:47 -0500

    File Input
    Modified to read input from a file.

--- Project_anthony_giuliano.java
+++ Project_anthony_giuliano.java
@@ -1,8 +1,10 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
 
 public class Project_anthony_giuliano {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         // Establish & declare variables
         int policyNumber = 0, age = 0;
@@ -10,58 +12,66 @@ public class Project_anthony_giuliano {
         boolean smoker = false;
         double height = 0, weight = 0, bodyMass = 0;
 
-        // Set up scanner for keyboard input
-        Scanner keyboard = new Scanner(System.in);
+        // Set up file object
+        // Chose to specify full file pathname due to CSC251Project folder not being recognized when using just the filename.
+        File myFile = new File("C:/Users/Antho/OneDrive/Documents/CPCC School/Programming Files/CSC251 Advanced JAVA Programming/GitHub/CSC251Project/PolicyInformation.txt");
 
-        // Begin user prompts & input
-        System.out.print("Please enter the Policy Number: ");
-        policyNumber = keyboard.nextInt();
-        keyboard.nextLine(); // Clear out keyboard buffer
+        // Set up scanner for file input
+        Scanner readFile = new Scanner(myFile);
 
-        System.out.print("\nPlease enter the Provider Name: ");
-        provider = keyboard.nextLine();
+        while(readFile.hasNext())
+        {
+            // Create ArrayList to hold all the policies
+            ArrayList<Policy> policyList = new ArrayList<Policy>();
 
-        System.out.print("\nPlease Enter the Policyholder's First Name: ");
-        firstName = keyboard.nextLine();
 
-        System.out.print("\nPlease enter the Policyholder's Last Name: ");
-        lastName = keyboard.nextLine();
+            // Collect input from file
+            policyNumber = readFile.nextInt();
+            readFile.nextLine(); // Clear out keyboard buffer
+           
+            provider = readFile.nextLine();
+            firstName = readFile.nextLine();
+            lastName = readFile.nextLine();
+            age = readFile.nextInt();
+            readFile.nextLine(); // buffer clear
+            
+            smokeStatus = readFile.nextLine();
+            height = readFile.nextDouble();
+            weight = readFile.nextDouble();
 
-        System.out.print("\nPlease enter the Policyholder's Age: ");
-        age = keyboard.nextInt();
-        keyboard.nextLine(); // buffer clear
+            // Set smoker boolean variable using string input from user
+            if (smokeStatus.equalsIgnoreCase("smoker"))
+            {   
+                smoker = true;
+            }
+            else if (smokeStatus.equalsIgnoreCase("non-smoker"))
+            {
+                smoker = false;
+            }
+            // Create Policy object using input data
+            Policy userPolicy = new Policy(policyNumber, provider, firstName, lastName, age, smoker, height, weight);
 
-        System.out.print("\nPlease enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-        smokeStatus = keyboard.nextLine();
+            // Add policy to policyList ArrayList
+            policyList.add(userPolicy);
 
-        System.out.print("\nPlease enter the Policyholder's Height (in inches): ");
-        height = keyboard.nextDouble();
+            // Determine BMI and store to variable for output
+            bodyMass = userPolicy.calcBMI();
 
-        System.out.print("\nPlease enter the Policyholder's weight (in pounds): ");
-        weight = keyboard.nextDouble();
-
-        // Set smoker boolean variable using string input from user
-        if (smokeStatus.equalsIgnoreCase("smoker"))
-            smoker = true;
-        else if (smokeStatus.equalsIgnoreCase("non-smoker"))
-            smoker = false;
-
-        // Create Policy object using input data
-        Policy userPolicy = new Policy(policyNumber, provider, firstName, lastName, age, smoker, height, weight);
-
-        // Determine BMI and store to variable for output
-        bodyMass = userPolicy.calcBMI();
-
-        // Format and display output
-        System.out.println("Policy Number: " + userPolicy.getPolicyNumber());
-        System.out.println("Provider Name: " + userPolicy.getProviderName());
-        System.out.println("Policyholder's First Name: " + userPolicy.getFirstName());
-        System.out.println("Policyholder's Last Name: " + userPolicy.getLastName());
-        System.out.println("Policyholder's Age: " + userPolicy.getAge());
-        System.out.println("Policyholder's Smoking Status: " + smokeStatus);
-        System.out.printf("Policyholder's Height: %.1f", userPolicy.getHeight());
-        System.out.printf("\nPolicyholder's Weight: %.1f", userPolicy.getWeight());
-        System.out.printf("\nPolicyholder's BMI: %.2f", bodyMass);
-        System.out.printf("\nPolicy Price: $%.2f", userPolicy.policyCost(bodyMass));
+            // Format and display output
+            System.out.println("Policy Number: " + userPolicy.getPolicyNumber());
+            System.out.println("Provider Name: " + userPolicy.getProviderName());
+            System.out.println("Policyholder's First Name: " + userPolicy.getFirstName());
+            System.out.println("Policyholder's Last Name: " + userPolicy.getLastName());
+            System.out.println("Policyholder's Age: " + userPolicy.getAge());
+            System.out.println("Policyholder's Smoking Status: " + smokeStatus);
+            System.out.printf("Policyholder's Height: %.1f inches", userPolicy.getHeight());
+            System.out.printf("\nPolicyholder's Weight: %.1f pounds", userPolicy.getWeight());
+            System.out.printf("\nPolicyholder's BMI: %.2f", bodyMass);
+            System.out.printf("\nPolicy Price: $%.2f", userPolicy.policyCost(bodyMass));
+            System.out.print("\n\n");
+        }
+       
+        // Close file scanner
+        readFile.close();
     }
 }
